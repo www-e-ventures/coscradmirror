8296c3cede1104e9b50ab7f78df260ff69afa320
Aaron Plahn <aplahn85@gmail.com>
2021-08-01 10:26:55 -0700

    build: update arango db startup script

--- scripts/arangodb/start-arango-db.sh
+++ scripts/arangodb/start-arango-db.sh
@@ -5,18 +5,18 @@ set -e
 # Remember to set the following environment variables and to add their values to your .env in project root.
 # ARANGO_DB_SERVER="dbserver";
 # ARANGO_DB_ROOT_PASSWORD="rootPASSWORD";
-# ARRANGO_DB_USER="devtester";
-# ARRANGO_DB_USER_PASSWORD="confidential"
-# ARRANGO_DB_NAME="testdb";
+# ARANGO_DB_USER="devtester";
+# ARANGO_DB_USER_PASSWORD="confidential"
+# ARANGO_DB_NAME="testdb";
 
-echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
-(docker kill $SERVER || :) && \
-  (docker rm $SERVER || :) && \
-  docker run -e ARANGO_ROOT_PASSWORD=$ARANGO_DB_PASSWORD -p 8529:$ARANGO_DB_PORT -d arangodb
+echo "echo stop & remove old docker [$ARANGO_DB_SERVER] and starting new fresh instance of [$ARANGO_DB_SERVER]"
+(docker kill $ARANGO_DB_SERVER || :) && \
+  (docker rm $ARANGO_DB_SERVER || :) && \
+  docker run -e ARANGO_ROOT_PASSWORD=$ARANGO_DB_ROOT_PASSWORD --name $ARANGO_DB_SERVER -p $ARANGO_DB_PORT:8529 -d arangodb
 
 # wait for pg to start
-echo "sleep wait for arango-db-server [$SERVER] to start";
-SLEEP 3;
+echo "sleep wait for arango-db-server [$ARANGO_DB_SERVER] to start";
+sleep 3;
 
 # TODO
 # create the db 
