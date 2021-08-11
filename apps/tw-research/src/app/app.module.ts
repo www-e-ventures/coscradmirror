b3d019ae99b9d89855e58ca47d21cf9cf658aae2
Aaron Plahn <aplahn85@gmail.com>
2021-08-10 19:42:00 -0700

    build: add auth0-angular

--- apps/tw-research/src/app/app.module.ts
+++ apps/tw-research/src/app/app.module.ts
@@ -1,12 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AuthModule } from '@auth0/auth0-angular';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'dev-ne4y17z9.auth0.com',
+      clientId: '8l2uydFmXezw3KooIfqjGPIYIWQLF2he',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
