7976deeb374a64576b8f2778a1f706f795194c7a
Aaron Plahn <aplahn85@gmail.com>
2021-08-10 19:18:31 -0700

    refactor(tw-research): rename frontend project

--- apps/tw-research-e2e/src/integration/app.spec.ts
+++ apps/tw-research-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('tw-researc', () => {
+describe('tw-research', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('tw-researc', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to tw-researc!');
+    getGreeting().contains('Welcome to tw-research!');
   });
 });
--- apps/tw-research/jest.config.js
+++ apps/tw-research/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'tw-researc',
+  displayName: 'tw-research',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/tw-researc',
+  coverageDirectory: '../../coverage/apps/tw-research',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
