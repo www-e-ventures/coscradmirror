014bb0ff17ea2a054c0d056694b12d9baf59a9ba
Justin Bambrick <jbambrick@tsilhqotin.ca>
2021-11-23 15:29:47 -0800

    style(dictionary): generate general theme

---
+++ apps/nativescript-tng-dictionary-mobile/src/features/home/components/pages/vocabulary-list-detail/vocabulary-list-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'coscrad-vocabulary-list-detail',
+  templateUrl: './vocabulary-list-detail.component.html',
+  styleUrls: ['./vocabulary-list-detail.component.css']
+})
+export class VocabularyListDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/nativescript-tng-dictionary-mobile/src/features/home/home.module.ts
+++ apps/nativescript-tng-dictionary-mobile/src/features/home/home.module.ts
@@ -1,25 +1,26 @@
 import {
+  CUSTOM_ELEMENTS_SCHEMA,
   NgModule,
   NO_ERRORS_SCHEMA,
-  CUSTOM_ELEMENTS_SCHEMA
 } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from '@nativescript/angular';
-
 import { SharedModule } from '../shared/shared.module';
-import { HOME_COMPONENTS, HomeComponent } from './components';
+import { HomeComponent, HOME_COMPONENTS } from './components';
+import { VocabularyListDetailComponent } from './components/pages/vocabulary-list-detail/vocabulary-list-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  }
+    component: HomeComponent,
+  },
+  { path: 'vocab', component: VocabularyListDetailComponent },
 ];
 
 @NgModule({
   imports: [SharedModule, NativeScriptRouterModule.forChild(routes)],
-  declarations: [...HOME_COMPONENTS],
+  declarations: [...HOME_COMPONENTS, VocabularyListDetailComponent],
   exports: [...HOME_COMPONENTS],
-  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule {}
