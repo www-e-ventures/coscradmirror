0fb8fc31cb0636358d29597631fbe0f43d6f6605
Aaron Plahn <aplahn85@gmail.com>
2021-11-27 20:45:06 -0800

    refactor(api): move files and add some TODOs

--- apps/api/src/domain/models/term/dto/update-term.dto.ts
+++ apps/api/src/domain/models/term/dto/update-term.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTermDto } from './create-term.dto';
 
+// TODO [refactor] remove in favor of `PartialDTO<Term>`
 export class UpdateTermDto extends PartialType(CreateTermDto) {}
--- apps/api/src/domain/types/all-entities.ts
+++ apps/api/src/domain/types/all-entities.ts
@@ -1,4 +1,10 @@
 import { CreateTermDto } from '../models/term/dto/create-term.dto';
 import { CreateVocabularyListDto } from '../vocabulary-list/dto/create-vocabulary-list.dto';
 
+/**
+ * TODO [refactor] Remove this in favor of
+ * ```js
+ * PartialDTO<Entity>
+ * ```
+ */
 export type AllCreateEntityDtosUnion = CreateTermDto | CreateVocabularyListDto;
