c0f4096f678e4da27265d927b1ecd45bf0daa04c
Web Admin <webadmin@localhost.localdomain>
2021-12-11 14:56:05 -0500

    FIXED: Import test data file into arangosh JS script

--- scripts/arango/setup.js
+++ scripts/arango/setup.js
@@ -13,21 +13,24 @@ db._useDatabase(process.env.ARANGO_DB_NAME);
 require("internal").load(process.env.ARANGO_DOCKER_VOLUME_SCRIPTS_DIR + "/data/testData.js");
 
 // Add Collections
-var success = [];
+var success = false;
 testData.forEach(({collection: collectionName,databaseDTOs})=>{
   print(`Attempting to add collection "${collectionName}" to ${process.env.ARANGO_DB_NAME}`);
   if (db._create(collectionName)) {
     print('Created Collection');
-    success.push({ collectionName: collectionName, success:  true });
+    success = true;
   }
   else {
-    success.push({ collectionName: collectionName, success:  false });
+    success = false;
   }
 
-  if (process.env.SETUP_MODE == "dev") {
-    print(`Attempting to add test data for collection ${collectionName}`)
-    print(`Adding test data for collection ${collectionName}`)
-    databaseDTOs.forEach(dto => db._collection(collectionName).save(dto))
+  if (process.env.SETUP_MODE == "development") {
+    print(`MODE=development: Load test data`);
+    print(success);
+    if (success) {
+      print(`Attempting to add test data for collection ${collectionName}`)
+      databaseDTOs.forEach(dto => db._collection(collectionName).save(dto))
+    }
   }
 })
 
@@ -35,8 +38,11 @@ print('\nCollections Loaded:\n')
 var collectionsList = db._collections();
 
 collectionsList.forEach(col => {
-  // print(col.name());
   if (col.name().indexOf('_') != 0) {
     print(col.name());
+    const loadedData = db._query(`FOR d IN ${col.name()} RETURN d`);
+    for (const result of loadedData) {
+      print(result);
+    }
   }
 });
