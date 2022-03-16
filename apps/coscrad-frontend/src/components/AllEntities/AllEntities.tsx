26166fd39eaba36cde60deede180a36b6369d526
Geoff <newsopkodon@gmail.com>
2022-03-16 16:03:58 -0700

    WIP Make request for entity descriptions

--- apps/coscrad-frontend/src/components/AllEntities/AllEntities.tsx
+++ apps/coscrad-frontend/src/components/AllEntities/AllEntities.tsx
@@ -1,19 +1,39 @@
+import { useEffect, useState } from 'react';
 import './AllEntities.module.scss';
 
 /* eslint-disable-next-line */
-export interface AllEntitiesProps {}
+export interface AllEntitiesProps { }
 
-const entityData = {
-  term: "term description",
-  vocabularyList: "A vocabulary list gathers terms with filters that apply within the context of the vocabulary list",
-  tag: "A tag is a classifier for an entity or a pair of related entities"
+type EntitiesAndDescriptions = Record<string, string>;
+
+type ComponentState = {
+  entitiesAndDescriptions: EntitiesAndDescriptions
 }
 
 export function AllEntities(props: AllEntitiesProps) {
+  const [appState, setAppState] = useState<ComponentState>({
+    entitiesAndDescriptions: {}
+  });
+
+  if (Object.keys(appState.entitiesAndDescriptions).length === 0)
+    return (
+      <div>Loading...</div>
+    )
+
+  useEffect(() => {
+    setAppState({ entitiesAndDescriptions: {} });
+    const apiUrl = `http://104.225.142.106:3131/api/entities/descriptions/`;
+    fetch(apiUrl, { mode: 'cors' })
+      .then((res) => res.json())
+      .then((entitiesAndDescriptions) => {
+        setAppState({ ...appState, entitiesAndDescriptions });
+      }).catch(rej => console.log(rej))
+  }, [setAppState]);
+
   return (
     <div>
       <h1>Welcome to AllEntities!</h1>
-      <p>{Object.entries(entityData).map(([entityType,description])=>(<p>{entityType}:{description}</p>))}</p>
+      <p>{Object.entries(appState.entitiesAndDescriptions).map(([entityType, description]) => (<p><b>{entityType}:</b>&nbsp;{description}</p>))}</p>
     </div>
   );
 }
