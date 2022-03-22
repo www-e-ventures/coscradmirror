69a1ceaf469af1b02e12ec8cf73b90bfaf6b0c4b
Aaron Plahn <aplahn85@gmail.com>
2022-03-22 09:00:59 -0700

    test(coscrad-frontend): fix failing tests

--- apps/coscrad-frontend/src/components/AllEntities/AllEntities.spec.tsx
+++ apps/coscrad-frontend/src/components/AllEntities/AllEntities.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import AllEntities from './AllEntities';
 
 describe('AllEntities', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<AllEntities />);
+    const { baseElement } = render(<MemoryRouter><AllEntities /></MemoryRouter>);
     expect(baseElement).toBeTruthy();
   });
 });
