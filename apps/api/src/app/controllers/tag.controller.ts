0c6ec7f5441f56a8d849d619923f65b0e1c0d0c5
Aaron Plahn <aplahn85@gmail.com>
2022-05-03 14:33:34 -0700

    refactor(api): create dedicated query endpoints for tags

--- apps/api/src/app/controllers/tag.controller.ts
+++ apps/api/src/app/controllers/tag.controller.ts
@@ -1,9 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Tag } from '../../domain/models/tag/tag.entity';
+import { isResourceId } from '../../domain/types/ResourceId';
+import { InternalError, isInternalError } from '../../lib/errors/InternalError';
+import { isNotFound } from '../../lib/types/not-found';
+import cloneToPlainObject from '../../lib/utilities/cloneToPlainObject';
 import { RepositoryProvider } from '../../persistence/repositories/repository.provider';
+import { TagViewModel } from '../../view-models/buildViewModelForResource/viewModels';
+import httpStatusCodes from '../constants/httpStatusCodes';
 
 @Controller('tags')
-export class AddTagController {
+export class TagController {
     constructor(private readonly repositoryProvider: RepositoryProvider) {}
+
+    @Get('/:id')
+    async fetchById(@Res() res, @Param('id') id: string) {
+        if (!isResourceId(id))
+            return res
+                .status(httpStatusCodes.badRequest)
+                .send(new InternalError(`Invalid tag ID: ${id}`));
+
+        const result = await this.repositoryProvider.getTagRepository().fetchById(id);
+
+        if (isInternalError(result)) return res.status(httpStatusCodes.badRequest).send(result);
+
+        if (isNotFound(result)) return res.status(httpStatusCodes.notFound).send();
+
+        const viewModel = cloneToPlainObject(new TagViewModel(result));
+
+        return res.status(httpStatusCodes.ok).send(viewModel);
+    }
+
+    @Get('')
+    async fetchMay(@Res() res) {
+        const result = await this.repositoryProvider.getTagRepository().fetchMany();
+
+        const allErrors = result.filter(isInternalError);
+
+        if (allErrors.length > 0) return res.status(httpStatusCodes.internalError).send(allErrors);
+
+        // Can we make the above condition a typeguard and avoid casting?
+        const allTags = result as Tag[];
+
+        const viewModels = allTags.map((tag) => new TagViewModel(tag));
+
+        return res.status(httpStatusCodes.ok).send(viewModels);
+    }
 
     // @UseGuards(AuthGuard('jwt'))
     // @Post()
