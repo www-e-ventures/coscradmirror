9bbcbee69c1a643976270852d712b811a189eb97
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-05-05 09:43:47 -0700

    feat(tng-language-hub): add responsive navbar

---
+++ apps/tsilhqotin-language-hub/src/Components/Widgets/Navbar/dropdown.tsx
@@ -0,0 +1,40 @@
+export interface AboutProps {}
+
+export function Dropdown() {
+    return (
+        <div>
+            <a className="menu__item" href="/About">
+                About
+            </a>
+
+            <a className="menu__item" href="/Apps">
+                Apps
+            </a>
+
+            <a className="menu__item" href="/Songs">
+                Songs
+            </a>
+
+            <a className="menu__item" href="/Videos">
+                Videos
+            </a>
+
+            <a className="menu__item" href="/Teachers">
+                Teachers
+            </a>
+
+            <a className="menu__item" href="/Funders">
+                Funders
+            </a>
+
+            <a className="menu__item" href="/Greetings">
+                Greetings
+            </a>
+            <a className="menu__item" href="/Links">
+                Links
+            </a>
+        </div>
+    );
+}
+
+export default Dropdown;
