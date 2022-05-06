4a7a800077e5f5ae330fe939fb26f499d0098631
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-05-06 08:57:46 -0700

    style(tng-language-hub): add divider to links and funders

--- apps/tsilhqotin-language-hub/src/Components/Pages/Funders/Funders.tsx
+++ apps/tsilhqotin-language-hub/src/Components/Pages/Funders/Funders.tsx
@@ -1,3 +1,4 @@
+import { Divider } from '@mui/material';
 import './Funders.module.css';
 
 /* eslint-disable-next-line */
@@ -11,96 +12,102 @@ export function Funders(props: FundersProps) {
                     <h1 id="pageTitle">Funders</h1>
                 </div>
             </div>
-            <h2 style={{ textAlign: 'center' }}>Language Program</h2>
-            <h3>National Research Council</h3>
-            <p>
-                <a href="https://nrc.canada.ca/en" target={'_blank'} rel="noreferrer">
-                    Here is a link to the NRC website.
-                </a>
-            </p>
-            <h3>Listen Hear Our Voices</h3>
-            <p>
-                <a
-                    href="https://www.bac-lac.gc.ca/eng/discover/aboriginal-heritage/initiatives/listen/Pages/default.aspx"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Here is a link to the LHOV website.
-                </a>
-            </p>
+            <div className="pageContent">
+                <h2 style={{ textAlign: 'center' }}>Language Program</h2>
+                <Divider />
 
-            <h2 style={{ textAlign: 'center' }}>Community Radio Project</h2>
+                <h3>National Research Council</h3>
+                <p>
+                    <a href="https://nrc.canada.ca/en" target={'_blank'} rel="noreferrer">
+                        Here is a link to the NRC website.
+                    </a>
+                </p>
+                <h3>Listen Hear Our Voices</h3>
+                <p>
+                    <a
+                        href="https://www.bac-lac.gc.ca/eng/discover/aboriginal-heritage/initiatives/listen/Pages/default.aspx"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Here is a link to the LHOV website.
+                    </a>
+                </p>
 
-            <h3>Northern Aboriginal Broadcasting</h3>
-            <p>
-                <a
-                    href="https://www.canada.ca/en/canadian-heritage/services/funding/aboriginal-peoples/northern-broadcasting.html"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Here is a link to the NAB website.
-                </a>
-            </p>
-            <h3>First Peoples Cultural Council</h3>
-            <p>
-                <a href="http://www.fpcc.ca/" target="_blank" rel="noreferrer">
-                    Here is a link to the FPCC website.
-                </a>
-            </p>
-            <h3>Cariboo Chilcotin Aboriginal Training Employment Centre</h3>
-            <p>
-                <a href="http://www.ccatec.com/" target="_blank" rel="noreferrer">
-                    Here is a link to CCATEC's website.
-                </a>
-            </p>
+                <h2 style={{ textAlign: 'center' }}>Community Radio Project</h2>
+                <Divider />
 
-            <h2 style={{ textAlign: 'center' }}>Previous Funders</h2>
+                <h3>Northern Aboriginal Broadcasting</h3>
+                <p>
+                    <a
+                        href="https://www.canada.ca/en/canadian-heritage/services/funding/aboriginal-peoples/northern-broadcasting.html"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Here is a link to the NAB website.
+                    </a>
+                </p>
+                <h3>First Peoples Cultural Council</h3>
+                <p>
+                    <a href="http://www.fpcc.ca/" target="_blank" rel="noreferrer">
+                        Here is a link to the FPCC website.
+                    </a>
+                </p>
+                <h3>Cariboo Chilcotin Aboriginal Training Employment Centre</h3>
+                <p>
+                    <a href="http://www.ccatec.com/" target="_blank" rel="noreferrer">
+                        Here is a link to CCATEC's website.
+                    </a>
+                </p>
 
-            <h3>Indigitization</h3>
-            <p>
-                “The{' '}
-                <a href="https://www.indigitization.ca/" target="_blank" rel="noreferrer">
-                    Indigitization Grant Program
-                </a>{' '}
-                is a grant funding opportunity sponsored by the Irving K. Barber Learning Centre at
-                the University of British Columbia, Museum of Anthropology at the University of
-                British Columbia, and the First Nations Technology Council.”
-            </p>
+                <h2 style={{ textAlign: 'center' }}>Previous Funders</h2>
+                <Divider />
 
-            <h3>BC Capacity Initiative</h3>
-            <p>
-                Our work has previously been funded in part through the{' '}
-                <a href="https://www.bccapacity.org/" target="_blank" rel="noreferrer">
-                    BC Capacity Initiative
-                </a>{' '}
-                (BCCI).
-            </p>
+                <h3>Indigitization</h3>
+                <p>
+                    “The{' '}
+                    <a href="https://www.indigitization.ca/" target="_blank" rel="noreferrer">
+                        Indigitization Grant Program
+                    </a>{' '}
+                    is a grant funding opportunity sponsored by the Irving K. Barber Learning Centre
+                    at the University of British Columbia, Museum of Anthropology at the University
+                    of British Columbia, and the First Nations Technology Council.”
+                </p>
 
-            <h3>Simon Fraser University First Nations Languages Centre</h3>
-            <p>
-                We partnered with{' '}
-                <a href="https://www.sfu.ca/fnlc.html" target="_blank" rel="noreferrer">
-                    SFU
-                </a>{' '}
-                as part of the 7 year project, First Nations Languages in the Twenty-first Century:
-                Looking Back, Looking Forward. This project was funded by a partnership grant from
-                the Social Sciences and Humanities Research Council (SSHRC).
-            </p>
+                <h3>BC Capacity Initiative</h3>
+                <p>
+                    Our work has previously been funded in part through the{' '}
+                    <a href="https://www.bccapacity.org/" target="_blank" rel="noreferrer">
+                        BC Capacity Initiative
+                    </a>{' '}
+                    (BCCI).
+                </p>
 
-            <h3>New Relationship Trust</h3>
-            <p style={{ margin: '0' }}>
-                The language videos "Ts’eman Ts’elhgen" and “ʔEts’eteqash” were produced by William
-                Myers as part of a project funded through the{' '}
-                <a
-                    href="https://www.newrelationshiptrust.ca/initiatives/elders-youth/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    {' '}
-                    New Relationship Trust BC First Nation Elders & Youth Grant Initiative
-                    2017-2018.
-                </a>
-            </p>
+                <h3>Simon Fraser University First Nations Languages Centre</h3>
+                <p>
+                    We partnered with{' '}
+                    <a href="https://www.sfu.ca/fnlc.html" target="_blank" rel="noreferrer">
+                        SFU
+                    </a>{' '}
+                    as part of the 7 year project, First Nations Languages in the Twenty-first
+                    Century: Looking Back, Looking Forward. This project was funded by a partnership
+                    grant from the Social Sciences and Humanities Research Council (SSHRC).
+                </p>
+
+                <h3>New Relationship Trust</h3>
+                <p style={{ margin: '0' }}>
+                    The language videos "Ts’eman Ts’elhgen" and “ʔEts’eteqash” were produced by
+                    William Myers as part of a project funded through the{' '}
+                    <a
+                        href="https://www.newrelationshiptrust.ca/initiatives/elders-youth/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {' '}
+                        New Relationship Trust BC First Nation Elders & Youth Grant Initiative
+                        2017-2018.
+                    </a>
+                </p>
+            </div>
         </div>
     );
 }
