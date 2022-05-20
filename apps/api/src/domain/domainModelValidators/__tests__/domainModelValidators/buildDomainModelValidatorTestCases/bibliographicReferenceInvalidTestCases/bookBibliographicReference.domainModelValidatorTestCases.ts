5940d51258b769e092bc65ba8cfe062da17d52e1
yaanahuu2 <gahorner@gmail.com>
2022-05-20 08:45:23 -0400

    feat(api): add book bibliographic model (#74)
    WIP add creators property to book bibliographic reference model
    
    WIP add reference to bibliographic reference in test dtos
    
    WIP added ? to optional properties

--- apps/api/src/domain/domainModelValidators/__tests__/domainModelValidators/buildDomainModelValidatorTestCases/bibliographicReferenceInvalidTestCases/bookBibliographicReference.domainModelValidatorTestCases.ts
+++ apps/api/src/domain/domainModelValidators/__tests__/domainModelValidators/buildDomainModelValidatorTestCases/bibliographicReferenceInvalidTestCases/bookBibliographicReference.domainModelValidatorTestCases.ts
@@ -1,3 +1,5 @@
+import BookBibliographicReferenceData from 'apps/api/src/domain/models/bibliographic-reference/entities/BookBibliographicReferenceData';
+import { DTO } from 'apps/api/src/types/DTO';
 import { EntityId } from '../../../../../../domain/types/ResourceId';
 import { resourceTypes } from '../../../../../../domain/types/resourceTypes';
 import { InternalError } from '../../../../../../lib/errors/InternalError';
@@ -15,13 +17,122 @@ const buildTopLevelError = (id: EntityId, innerErrors: InternalError[]): Interna
 export const buildBookBibliographicReferenceTestCases =
     (): DomainModelValidatorInvalidTestCase<BookBibliographicReference>[] => [
         {
-            description: 'The title is an empty string',
+            description: 'The Book Bibliographic Reference title is an empty string',
             invalidDTO: {
                 ...validDto,
                 data: {
                     ...validDto.data,
                     title: '',
-                },
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference creators array is empty',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    creators: [],
+                    // TODO remove cast
+                } as unknown as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference abstract is an empty string',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    abstract: '',
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference year is too big',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    year: 2100,
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference publisher is an empty string',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    publisher: '',
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference place is an empty string',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    place: '',
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference url is an invalid URL',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    url: '',
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference ISBN is an invalid ISBN',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    isbn: '',
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description:
+                'The Book Bibliographic Reference number of pages is not a positive integer',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    numberOfPages: -34,
+                    // TODO remove cast
+                } as DTO<BookBibliographicReferenceData>,
+            },
+            expectedError: buildTopLevelError(validDto.id, []),
+        },
+        {
+            description: 'The Book Bibliographic Reference number of pages is not a number',
+            invalidDTO: {
+                ...validDto,
+                data: {
+                    ...validDto.data,
+                    numberOfPages: '347',
+                    // TODO remove cast
+                } as unknown as DTO<BookBibliographicReferenceData>,
             },
             expectedError: buildTopLevelError(validDto.id, []),
         },
--- libs/validation/src/index.ts
+++ libs/validation/src/index.ts
@@ -1,7 +1,7 @@
 import buildSimpleValidator from './lib/buildSimpleValidationFunction';
 import DiscriminatedUnionValidator from './lib/DiscriminatedUnionValidator';
 
-export { IsOptional } from 'class-validator';
+export { ArrayNotEmpty, IsInt, IsISBN, IsOptional, IsPositive, IsUrl } from 'class-validator';
 export * from './lib/decorators';
 export * from './lib/interfaces';
 export { buildSimpleValidator, DiscriminatedUnionValidator };
