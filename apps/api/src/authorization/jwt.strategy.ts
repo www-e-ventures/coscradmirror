c8c5b7013a8aafbb5a4cecea958aad5bf92ceadf
Aaron Plahn <aplahn85@gmail.com>
2022-07-05 17:59:44 -0700

    docs(api): update swagger to attach auth headers (#122)

--- apps/api/src/authorization/jwt.strategy.ts
+++ apps/api/src/authorization/jwt.strategy.ts
@@ -3,34 +3,43 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { passportJwtSecret } from 'jwks-rsa';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { InternalError } from '../lib/errors/InternalError';
+
+type HasSub = { sub: string };
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
-    const issuerURL = configService.get<string>('AUTH0_ISSUER_URL');
-    if (!issuerURL)
-      throw new Error('Internal Error: could not determine issuer url');
+    constructor(configService: ConfigService) {
+        const issuerURL = configService.get<string>('AUTH0_ISSUER_URL');
+        if (!issuerURL) throw new Error('Internal Error: could not determine issuer url');
 
-    const audience = configService.get<string>('AUTH0_AUDIENCE');
-    if (!audience)
-      throw new Error('Internal Error: could not determine audience');
+        const audience = configService.get<string>('AUTH0_AUDIENCE');
+        if (!audience) throw new Error('Internal Error: could not determine audience');
 
-    super({
-      secretOrKeyProvider: passportJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${issuerURL}.well-known/jwks.json`,
-      }),
+        const jwksUri = `${issuerURL}.well-known/jwks.json`;
 
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: 'https://tw-research.tsilhqotinlanguage.ca',
-      issuer: issuerURL,
-      algorithms: ['RS256'],
-    });
-  }
+        super({
+            secretOrKeyProvider: passportJwtSecret({
+                cache: true,
+                rateLimit: true,
+                jwksRequestsPerMinute: 5,
+                jwksUri,
+            }),
 
-  validate(payload: unknown): unknown {
-    return payload;
-  }
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            audience,
+            issuer: issuerURL,
+            algorithms: ['RS256'],
+        });
+    }
+
+    validate(payload: unknown): unknown {
+        const userId = (payload as HasSub)?.sub;
+
+        if (!userId) {
+            throw new InternalError(`Invalid user payload received from auth0: ${payload}`);
+        }
+
+        return { id: userId };
+    }
 }
