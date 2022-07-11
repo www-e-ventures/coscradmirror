d715426f6ebb8425b814098ccfd57663b5b85277
Aaron Plahn <aplahn85@gmail.com>
2022-07-11 15:21:50 -0700

    fix(api): fix default db connection url resolution (#130)

--- apps/api/src/persistence/database/arango-connection.provider.ts
+++ apps/api/src/persistence/database/arango-connection.provider.ts
@@ -54,7 +54,7 @@ export class ArangoConnectionProvider {
             dbHostUrl: buildFullHostURL(
                 this.configService.get<string>('ARANGO_DB_HOST_DOMAIN', 'localhost'),
                 this.configService.get<Scheme>('ARANGO_DB_HOST_SCHEME', Scheme.http),
-                this.configService.get<Port>('ARANGO_DB_HOST_PORT', '80')
+                this.configService.get<number>('ARANGO_DB_HOST_PORT', 80).toString() as Port
             ),
         });
 
