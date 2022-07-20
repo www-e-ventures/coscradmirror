2cf88c98d514d2b91a4410e1ecc4f30808f126b4
Aaron Plahn <aplahn85@gmail.com>
2022-07-20 16:42:46 -0700

    fix(tng-radio-hub): fix type issue in footerLogic

--- apps/tng-radio-hub/src/Components/widgets/footer/footerLogic.tsx
+++ apps/tng-radio-hub/src/Components/widgets/footer/footerLogic.tsx
@@ -1,26 +1,31 @@
-import { Column, Container, Link, Row, Titles, Wrapper } from './footer.module';
+import React, { ReactNode } from 'react';
+import { Column, Container, Link, Row, Titles, Wrapper } from "./footer.module";
 
-export default function Footer({ children, ...restProps }) {
+type HasChildren = {
+    children: ReactNode;
+}
+
+export default function Footer({ children, ...restProps }: HasChildren) {
     return <Container {...restProps}>{children}</Container>;
   }
   
-  Footer.Wrapper = function FooterWrapper({ children, ...restProps }) {
+  Footer.Wrapper = function FooterWrapper({ children, ...restProps }: HasChildren) {
     return <Wrapper {...restProps}>{children}</Wrapper>;
   };
   
-  Footer.Row = function FooterRow({ children, ...restProps }) {
+  Footer.Row = function FooterRow({ children, ...restProps }: HasChildren) {
     return <Row {...restProps}>{children}</Row>;
   };
   
-  Footer.Column = function FooterColumn({ children, ...restProps }) {
+  Footer.Column = function FooterColumn({ children, ...restProps }: HasChildren) {
     return <Column {...restProps}>{children}</Column>;
   };
   
-  Footer.Link = function FooterLink({ children, ...restProps }) {
+  Footer.Link = function FooterLink({ children, ...restProps }: HasChildren) {
     return <Link {...restProps}>{children}</Link>;
   };
   
-  Footer.Titles = function FooterTitles({ children, ...restProps }) {
+  Footer.Titles = function FooterTitles({ children, ...restProps }: HasChildren) {
     return <Titles {...restProps}>{children}</Titles>;
   };
   
