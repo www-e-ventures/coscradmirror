f7a1e01b6077dde70214891992e6fe1a0a72dd67
Aaron Plahn <aplahn85@gmail.com>
2022-07-21 11:48:31 -0700

    feat(api): introduce RBAC for commands (#148)
    * feat(api): introduce RBAC for commands
    
    * refactor based on PR (#148)

---
+++ apps/api/src/app/controllers/command/command-rbac.e2e.spec.ts
@@ -0,0 +1,195 @@
+import { CommandHandlerService, FluxStandardAction } from '@coscrad/commands';
+import { CoscradUserRole } from '@coscrad/data-types';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import getValidResourceInstanceForTest from '../../../domain/domainModelValidators/__tests__/domainModelValidators/utilities/getValidResourceInstanceForTest';
+import { IIdManager } from '../../../domain/interfaces/id-manager.interface';
+import { PublishSong } from '../../../domain/models/song/commands/publish-song.command';
+import { PublishSongCommandHandler } from '../../../domain/models/song/commands/publish-song.command-handler';
+import { CoscradUserWithGroups } from '../../../domain/models/user-management/user/entities/user/coscrad-user-with-groups';
+import buildDummyUuid from '../../../domain/models/__tests__/utilities/buildDummyUuid';
+import { ResourceType } from '../../../domain/types/ResourceType';
+import buildInMemorySnapshot from '../../../domain/utilities/buildInMemorySnapshot';
+import { ArangoConnectionProvider } from '../../../persistence/database/arango-connection.provider';
+import generateRandomTestDatabaseName from '../../../persistence/repositories/__tests__/generateRandomTestDatabaseName';
+import TestRepositoryProvider from '../../../persistence/repositories/__tests__/TestRepositoryProvider';
+import buildTestData from '../../../test-data/buildTestData';
+import httpStatusCodes from '../../constants/httpStatusCodes';
+import setUpIntegrationTest from '../__tests__/setUpIntegrationTest';
+
+/**
+ * This is a high level test that checks the Role Base Access Control for
+ * the commands endpoint. It mocks out the auth strategy / guard.
+ *
+ * `command.controller.e2e.spec.ts` tests the command controller in more detail
+ **/
+describe('Role Based Access Control for commands', () => {
+    let testRepositoryProvider: TestRepositoryProvider;
+
+    let app: INestApplication;
+
+    let commandHandlerService: CommandHandlerService;
+
+    let idManager: IIdManager;
+
+    let arangoConnectionProvider: ArangoConnectionProvider;
+
+    const existingSong = getValidResourceInstanceForTest(ResourceType.song).clone({
+        id: buildDummyUuid(),
+        published: false,
+    });
+
+    const validCommandFSA: FluxStandardAction<PublishSong> = {
+        type: 'PUBLISH_SONG',
+        payload: {
+            id: existingSong.id,
+        },
+    };
+
+    describe('when the user does not have an admin role', () => {
+        const ordinaryUser = buildTestData().users[0].clone({
+            roles: [CoscradUserRole.viewer],
+        });
+
+        const userGroup = buildTestData().userGroups[0].clone({
+            userIds: [ordinaryUser.id],
+        });
+
+        const testUserWithGroups = new CoscradUserWithGroups(ordinaryUser, [userGroup]);
+
+        beforeAll(async () => {
+            ({
+                testRepositoryProvider,
+                app,
+                commandHandlerService,
+                idManager,
+                arangoConnectionProvider,
+            } = await setUpIntegrationTest(
+                {
+                    ARANGO_DB_NAME: generateRandomTestDatabaseName(),
+                },
+                { shouldMockIdGenerator: true, testUserWithGroups }
+            ));
+
+            commandHandlerService.registerHandler(
+                'PUBLISH_SONG',
+                new PublishSongCommandHandler(testRepositoryProvider, idManager)
+            );
+
+            await testRepositoryProvider.addFullSnapshot(
+                buildInMemorySnapshot({
+                    users: [ordinaryUser],
+                    userGroups: [userGroup],
+                    resources: {
+                        song: [existingSong],
+                    },
+                })
+            );
+        });
+
+        afterAll(async () => {
+            await app.close();
+
+            await arangoConnectionProvider.dropDatabaseIfExists();
+        });
+        it('should return an unauthroized error', async () => {
+            await request(app.getHttpServer())
+                .post(`/commands`)
+                .send(validCommandFSA)
+                .expect(httpStatusCodes.unauthorized);
+        });
+    });
+
+    describe('when there is no user on the request (public request)', () => {
+        beforeAll(async () => {
+            ({ testRepositoryProvider, app, commandHandlerService, idManager } =
+                await setUpIntegrationTest(
+                    {
+                        ARANGO_DB_NAME: generateRandomTestDatabaseName(),
+                    },
+                    { shouldMockIdGenerator: true }
+                ));
+
+            commandHandlerService.registerHandler(
+                'PUBLISH_SONG',
+                new PublishSongCommandHandler(testRepositoryProvider, idManager)
+            );
+
+            await testRepositoryProvider.addFullSnapshot(
+                buildInMemorySnapshot({
+                    resources: {
+                        song: [existingSong],
+                    },
+                })
+            );
+        });
+
+        afterAll(async () => {
+            await app.close();
+
+            await arangoConnectionProvider.dropDatabaseIfExists();
+        });
+        it('should return an unauthroized error', async () => {
+            await request(app.getHttpServer())
+                .post(`/commands`)
+                .send(validCommandFSA)
+                .expect(httpStatusCodes.unauthorized);
+        });
+    });
+
+    (
+        [
+            [CoscradUserRole.projectAdmin, 'when the user is a project admin'],
+            [CoscradUserRole.superAdmin, 'when the user is a COSCRAD admin'],
+        ] as const
+    ).forEach(([role, description]) => {
+        describe(description, () => {
+            const adminUser = buildTestData().users[0].clone({
+                roles: [role],
+            });
+
+            const userGroup = buildTestData().userGroups[0].clone({
+                userIds: [adminUser.id],
+            });
+
+            const testUserWithGroups = new CoscradUserWithGroups(adminUser, [userGroup]);
+
+            beforeAll(async () => {
+                ({ testRepositoryProvider, app, commandHandlerService, idManager } =
+                    await setUpIntegrationTest(
+                        {
+                            ARANGO_DB_NAME: generateRandomTestDatabaseName(),
+                        },
+                        { shouldMockIdGenerator: true, testUserWithGroups }
+                    ));
+
+                commandHandlerService.registerHandler(
+                    'PUBLISH_SONG',
+                    new PublishSongCommandHandler(testRepositoryProvider, idManager)
+                );
+
+                await testRepositoryProvider.addFullSnapshot(
+                    buildInMemorySnapshot({
+                        users: [adminUser],
+                        userGroups: [userGroup],
+                        resources: {
+                            song: [existingSong],
+                        },
+                    })
+                );
+            });
+
+            afterAll(async () => {
+                await app.close();
+
+                await arangoConnectionProvider.dropDatabaseIfExists();
+            });
+            it('should return ok', async () => {
+                await request(app.getHttpServer())
+                    .post(`/commands`)
+                    .send(validCommandFSA)
+                    .expect(httpStatusCodes.ok);
+            });
+        });
+    });
+});
