e498d8af7deb04b04543347a7f71dedf05cb120a
Aaron Plahn <aplahn85@gmail.com>
2022-07-22 15:06:42 -0700

    General index component experimentation (#139)
    * refactor(tng-language-hub): add index component HOF
    
    * refactor(tng-language-hub): format files

--- apps/tsilhqotin-language-hub/src/Components/Pages/SongViewModel/SongViewModel.tsx
+++ apps/tsilhqotin-language-hub/src/Components/Pages/SongViewModel/SongViewModel.tsx
@@ -1,118 +1,22 @@
-import { Card } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { getConfig } from '../../../config';
+import buildIndexComponent from '../../../HigherOrderComponents/buildIndexCommponent/buildIndexComponent';
 
-export type SongData = {
-    title: string;
-
-    titleEnglish: string;
-
-    id: string;
-};
-
-type SongResult = {
-    data: SongData;
-};
-
-type ComponentState = {
-    songs: SongResult[];
-    searchContext: 'title';
-};
-
-const getData = async (endpoint: string) => fetch(endpoint).then((response) => response.json());
-
-/* eslint-disable-next-line */
-export interface SongViewModelProps {
-    songData?: SongData;
-}
-
-export function SongViewModel(props: SongViewModelProps) {
-    const [appState, setAppState] = useState<ComponentState>({
-        songs: [],
-        searchContext: 'title',
-    });
-    const [searchResults, setSearchResults] = useState({
-        selectedSongs: appState.songs,
-    });
-
-    useEffect(() => {
-        setAppState({ songs: [], searchContext: 'title' });
-        const apiUrl = `${getConfig().apiBaseUrl}/api/resources/songs`;
-        fetch(apiUrl, { mode: 'cors' })
-            .then((res) => res.json())
-            .then((songs) => {
-                setAppState({ ...appState, songs: songs });
-                setSearchResults({ selectedSongs: songs.data });
-            })
-            .catch((rej) => console.log(rej));
-    }, [setAppState]);
-
-    const rows: GridRowsProp = searchResults.selectedSongs
-        .map((result) => result.data)
-        .map((song) => ({
-            title: song.title,
-            titleEnglish: song.titleEnglish,
-            id: song.id,
-        }));
-
-    const columns: GridColDef[] = [
-        {
-            field: 'id',
-            headerName: 'ID',
-            renderCell: (idParam: GridRenderCellParams<string>) => (
-                <Link className="link" to={`/songs/${idParam.value}`}>
-                    <p>{idParam.value}</p>
-                </Link>
-            ),
-        },
-        {
-            field: 'title',
-            headerName: 'TITLE',
-            width: 100,
-        },
-        {
-            field: 'titleEnglish',
-            headerName: 'ENGLISH',
-            width: 150,
-            flex: 1,
-        },
-    ];
-
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="page">
-                <div id="heading">
-                    <div id="container">
-                        <h1 id="pageTitle">Songs</h1>
-                    </div>
-                </div>
-                <Card className="pageContent">
-                    <DataGrid
-                        sx={{ minHeight: '60vh', padding: '10px' }}
-                        rows={rows}
-                        columns={columns}
-                        rowsPerPageOptions={[10, 50, 100]}
-                        initialState={{
-                            pagination: {
-                                pageSize: 10,
-                            },
-                        }}
-                    />
-                </Card>
-            </div>
-        </ThemeProvider>
-    );
-}
-
-export default SongViewModel;
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: 'rgb(168,4,4)',
-        },
+const SongIndex = buildIndexComponent([
+    {
+        propertyKey: 'title',
+        heading: 'Title'
     },
-});
+    {
+        propertyKey: 'titleEnglish',
+        heading: 'Title (English)'
+    },
+    {
+        propertyKey: 'id',
+        heading: 'ID'
+    }
+],
+(id: string) => `${id}`,
+`${getConfig().apiBaseUrl}/api/resources/songs`
+);
+
+export default SongIndex;
---
+++ apps/tsilhqotin-language-hub/src/HigherOrderComponents/buildIndexCommponent/buildIndexComponent.tsx
@@ -0,0 +1,135 @@
+import { Card, createTheme, ThemeProvider } from '@mui/material';
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+type PropertyKeyAndHeading<TKeys> = {
+    propertyKey: TKeys;
+
+    heading: string;
+};
+
+type DetailPageLinkRenderer = (id: string) => string;
+
+type DetailAction = unknown;
+
+type IndexAction = unknown;
+
+type IndexQueryData<T> = {
+    data: T[];
+    actions: DetailAction[];
+};
+
+// TODO share this with the backend
+type IndexQueryResult<T> = {
+    data: IndexQueryData<T>;
+    actions: IndexAction[];
+};
+
+type IndexComponentState<T extends Record<string, unknown>> = {
+    viewModelResults: IndexQueryData<T>[];
+    searchContext: keyof T;
+};
+
+const buildStreamlinedViewmodelForTable = <T extends Record<string, unknown>, U extends keyof T>(
+    viewModel: T,
+    keysToKeep: U[]
+): Omit<T, U> =>
+    Object.entries(viewModel).reduce((acc: Omit<T, U>, [key, value]: [keyof T, unknown]) => {
+        if (!keysToKeep.includes(key as any)) return acc;
+
+        // @ts-expect-error fix this later
+        acc[key] = value;
+
+        return acc;
+    }, {} as Omit<T, U>);
+
+const buildIndexComponent = <T extends Record<string, unknown>>(
+    propertyKeysAndHeadings: PropertyKeyAndHeading<keyof T>[],
+    renderDetailLink: DetailPageLinkRenderer,
+    fetchManyEndpoint: string
+) => {
+    return () => {
+        const [appState, setAppState] = useState<IndexComponentState<T>>({
+            //  loading: false,
+            viewModelResults: [],
+            searchContext: 'title',
+        });
+
+        const [searchResults, setSearchResults] = useState({
+            selectedViewModels: appState.viewModelResults,
+        });
+
+        useEffect(() => {
+            // TODO generalize the search context piece
+            setAppState({ viewModelResults: [], searchContext: 'title' });
+
+            fetch(fetchManyEndpoint, { mode: 'cors' })
+                .then((res) => res.json())
+                .then((result) => {
+                    setAppState({ ...appState, viewModelResults: result });
+                    setSearchResults({ selectedViewModels: result.data });
+                })
+                .catch((rej) => console.log(rej));
+        }, [setAppState]);
+
+        // if (!appState.vocabularyLists || appState.vocabularyLists === []) return <Loading />
+
+        const rows: GridRowsProp = searchResults.selectedViewModels
+            .map((result) => result.data)
+
+            .map((viewModel) =>
+                buildStreamlinedViewmodelForTable(
+                    // @ts-expect-error fix this later
+                    viewModel,
+                    propertyKeysAndHeadings.map(({ propertyKey }) => propertyKey)
+                )
+            );
+
+        const columns: GridColDef[] = propertyKeysAndHeadings.map(({ propertyKey, heading }) => ({
+            // we know that we don't use symbol or number for view model property keys
+            field: propertyKey as string,
+            headerName: heading,
+            renderCell: (param: GridRenderCellParams<string>) => (
+                <Link className="link" to={renderDetailLink(param.row.id)}>
+                    <p>{param.value}</p>
+                </Link>
+            ),
+        }));
+
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="page">
+                    <div id="heading">
+                        <div id="container">
+                            <h1 id="pageTitle">Songs</h1>
+                        </div>
+                    </div>
+                    <Card className="pageContent">
+                        <DataGrid
+                            sx={{ minHeight: '60vh', padding: '10px' }}
+                            rows={rows}
+                            columns={columns}
+                            rowsPerPageOptions={[10, 50, 100]}
+                            initialState={{
+                                pagination: {
+                                    pageSize: 10,
+                                },
+                            }}
+                        />
+                    </Card>
+                </div>
+            </ThemeProvider>
+        );
+    };
+};
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: 'rgb(168,4,4)',
+        },
+    },
+});
+
+export default buildIndexComponent;
