c7456894401a66c74aaa250670306e2cfe9fcde4
Aaron Plahn <aplahn85@gmail.com>
2022-08-29 09:31:18 -0700

    feat(api): support POSITIVE_INTEGER data type (#165)

---
+++ libs/data-types/src/lib/decorators/PositiveInteger.ts
@@ -0,0 +1,16 @@
+import { IsPositiveInteger } from '../../../../validation/src';
+import { CoscradDataType } from '../types';
+import appendMetadata from '../utilities/appendMetadata';
+import mixinDefaultTypeDecoratorOptions from './common/mixinDefaultTypeDecoratorOptions';
+import { TypeDecoratorOptions } from './types/TypeDecoratorOptions';
+import WithValidation from './validation/WithValidation';
+
+export function PositiveInteger(userOptions?: Partial<TypeDecoratorOptions>): PropertyDecorator {
+    return (target: Object, propertyKey: string | symbol) => {
+        const options = mixinDefaultTypeDecoratorOptions(userOptions);
+
+        WithValidation(IsPositiveInteger(), options)(target, propertyKey);
+
+        appendMetadata(target, propertyKey, CoscradDataType.PositiveInteger, options);
+    };
+}
